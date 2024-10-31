de8be8b7f633e5adeb6a7845a59171d520208b0a
Tikiram Ruiz <tikiram@icloud.com>
2024-10-31 08:40:27 -0600

    fix: supported macOS version

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
       .iOS(.v17),
       .macCatalyst(.v17),
+      .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
